3029cb23b4958be183b48dfcef7f36944b6d49b4
Abhinav <abhinav.gupta@hotmail.com>
2018-06-11 12:06:56 -0700

    resync all files

--- app.py
+++ app.py
@@ -1,22 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, redirect
+from flask_pymongo import PyMongo
 
-import pymongo
+import mission_to_mars
 
 app = Flask(__name__)
 
-conn = 'mongodb://localhost:27017'
+mongo = PyMongo(app)
 
-client = pymongo.MongoClient(conn)
 
-db = client.missionToMars
-
-db.team.drop()
 @app.route('/')
 def index():
-    teams = list(db.team.find())
-    print(teams)
+    
+    mongo.db.collection.remove( { } )
+    mars_datasets = mongo.db.collection.find()
+    
+    data = mission_to_mars.title_scrape()
+    imagelink = mission_to_mars.image_link()
+    tweet_response = mission_to_mars.tweet_response()
+    table_content = mission_to_mars.table_data()
+    
+    
+    data_entry = {
+        "title" : data['title'],
+        "para" : data['paragraph'],
+        'image' : imagelink['src'],
+        'tweet_response' : tweet_response['today'],
+        'table' : table_content
+    
+    }
+    
+    mongo.db.collection.update(
+                               {},
+                               data_entry,
+                               upsert=True
+                               )
 
-    return render_template('index.html', teams=teams)
+return render_template('index.html', mars_datasets =  mars_datasets, table = table_content)
+
 
 
 if __name__ == "__main__":
